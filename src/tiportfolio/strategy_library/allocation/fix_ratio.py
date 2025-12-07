417cefc4b355c06e290e00679a92f33e01100c43
tim-hub <tim-hub@users.noreply.github.com>
2025-12-07 18:32:08 +1300

    make fix ratio test pass

--- src/tiportfolio/strategy_library/allocation/fix_ratio.py
+++ src/tiportfolio/strategy_library/allocation/fix_ratio.py
@@ -8,29 +8,28 @@ from tiportfolio.portfolio.allocation.frequency_based_allocation import (
     RebalanceFrequency,
 )
 from tiportfolio.portfolio.trading import Trading
-from tiportfolio.utils.get_previous_market_datetime import get_previous_market_open_day
 
 
-class FixPercentageFrequencyBasedAllocation(FrequencyBasedAllocation):
+class FixRatioFrequencyBasedAllocation(FrequencyBasedAllocation):
 
     def __init__(
             self,
             config: PortfolioConfig,
             strategies: List[Trading],
-            allocation_percentages: List[float],
+            allocation_ratio_list: List[float],
             rebalance_frequency: RebalanceFrequency,
             market_name: str = 'NYSE',
             hour: int = 9,
             minute: int = 30,
     ) -> None:
 
-        if len(allocation_percentages) != len(strategies):
+        if len(allocation_ratio_list) != len(strategies):
             raise ValueError(
                 "Length of allocation_percentages must match number of strategy_library"
             )
-        if not abs(sum(allocation_percentages) - 1.0) < 1e-6:
+        if not abs(sum(allocation_ratio_list) - 1.0) < 1e-6:
             raise ValueError("Sum of allocation_percentages must be 1.0")
-        self.allocation_percentages = allocation_percentages
+        self.allocation_percentages = allocation_ratio_list
         self.rebalance_frequency = rebalance_frequency
         super().__init__(
             config, strategies, rebalance_frequency, market_name, hour, minute
@@ -41,11 +40,10 @@ class FixPercentageFrequencyBasedAllocation(FrequencyBasedAllocation):
 
     def rebalance(self, current_step: Timestamp) -> None:
         for i in range(len(self.strategies)):
-            strategy = self.strategies[i]
-            target_allocation = self.allocation_percentages[i]
-            target_value = self.config.get('initial_capital') * target_allocation
+            self.strategy_quantity_map[
+                (current_step, str(self.strategies[i]))
+            ] = self.allocation_percentages[i]
 
-            close_price = strategy.dataframe.loc[current_step, 'close']
-            target_quantity = target_value / close_price
 
-            self.strategy_quantity_map.setdefault(current_step, {})[strategy.name] = target_quantity
+
+
